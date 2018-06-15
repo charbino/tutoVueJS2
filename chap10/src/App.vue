0c9db9869bcfd8327e0fe93254402e3e1270346a
sframinet <sebastien.framinet@asdoria.com>
2018-06-15 11:02:43 +0200

    Add chap 8-9-10

---
+++ chap10/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <app-quote-grid
+        :quotes="quotes"
+        >
+        </app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './component/QuoteGrid.vue';
+
+    export default {
+        data:function(){
+            return {
+                quotes: [
+                    'Juste a Qote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ chap10/src/component/QuoteGrid.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <app-Quote v-for="quote in quotes">{{ quote }}</app-Quote>
+    </div>
+</template>
+
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+
+<style scoped>
+
+
+</style>
